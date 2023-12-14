617881cc2b28ae7d45c9d493e64093b2dfa02e4a
Caio Vinicius Aguiar Alves <caiovini.aa@gmail.com>
2023-12-14 15:01:18 -0300

    Implemented a seed

--- Flappy Bird Clone/Assets/CameraMovementTest.cs
+++ Flappy Bird Clone/Assets/CameraMovementTest.cs
@@ -5,6 +5,6 @@ public class CameraMovementTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1, 0, 0) * (0.5f * Time.deltaTime);
+        transform.position += new Vector3(.5f, 0, 0) * (/*.5f*/ 1 * Time.deltaTime);
     }
 }
